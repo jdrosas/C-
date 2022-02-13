20e9735eac938a817faed227030453cf88de5d22
jdrosas <juandiazjdrd@gmail.com>
2022-02-12 20:53:29 -0500

    Agregar archivos de proyecto.

---
+++ PrimeraApp/Program.cs
@@ -0,0 +1,81 @@
+﻿Console.WriteLine("Hello, World!");
+/*
+int edad;
+edad = 21;
+
+Console.WriteLine(edad);
+
+Console.WriteLine("Multiplicación = " + edad * 23);
+Console.WriteLine("Suma = " + (edad + 23));
+Console.WriteLine("Resta = " + (edad - 23));
+Console.WriteLine("Decimales = " + 5.0/2.0);
+Console.WriteLine("Residuo = " + 9 % 4);
+
+Console.WriteLine($"Tu edad es = {++edad}");
+
+edad++;
+Console.WriteLine(edad);
+
+edad--;
+Console.WriteLine(edad);
+
+edad += 8;
+Console.WriteLine(edad);
+
+int edad2;
+int edad3;
+int edad4;
+
+edad2 = edad3 = edad4 = 98;
+
+Console.WriteLine("Edad persona 4 = " + edad4);
+
+var edad5 = 52;
+
+//edad5 = 53.2;
+
+Console.WriteLine(edad5);
+
+string nombre = "Juan";
+
+Console.WriteLine(nombre);*/
+
+//double numeroDouble = 5.5555;
+
+//int numeroInt;
+
+//numeroInt =(int) numeroDouble;
+
+
+
+//int habitantes = 10000;
+
+//long habitantes2022 = habitantes;
+
+//Console.WriteLine(habitantes);
+
+
+//float pesoCaja = 5.57F;
+
+//double pesoCincoUnidades = pesoCaja;
+
+//Console.WriteLine(pesoCincoUnidades);
+
+
+//Console.WriteLine("----------------Suma de numeros----------------");
+//Console.WriteLine("Introduce el primer valor numerico");
+//int num1 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Introduce el segundo valor numero");
+//int num2 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("El resultado es = " + (num1 + num2));
+
+const int VALOR = 21;
+
+const int VALOR2 = 22;
+
+Console.WriteLine("El valor de la constante es: {1}", VALOR, VALOR2);
+
+
+
